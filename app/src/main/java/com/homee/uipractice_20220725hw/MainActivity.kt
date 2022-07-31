544dce3d0c22c89f3767bec06476738b4ea4e235
신지 <wmfrjdnjfk12@gmail.com>
2022-07-31 13:46:14 +0900

    LoginUIPractice_20220726폴더명임

---
+++ app/src/main/java/com/homee/uipractice_20220725hw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.homee.uipractice_20220725hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
